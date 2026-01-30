3e3653a1c624df86cc4669d119586835708374c8
Xavier <xfavia@gmail.com>
2026-01-30 17:55:32 -0600

    Edit for frontend deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: [["babel-plugin-react-compiler"]],
-			},
-		}),
-	],
-	server: {
-		proxy: {
-			"/api": "http://127.0.0.1:8000",
-		},
-	},
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [
+			react({
+				babel: {
+					plugins: [["babel-plugin-react-compiler"]],
+				},
+			}),
+		],
+
+		server:
+			mode === "development"
+				? {
+						proxy: {
+							"/api": env.VITE_API_URL || "http://127.0.0.1:8000",
+						},
+					}
+				: undefined,
+	};
 });
