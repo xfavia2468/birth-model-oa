5589c081b8a299242526f4a0a81360dc317c9333
Xavier <xfavia@gmail.com>
2026-01-29 20:28:18 -0600

    Change django to pagination with 20 results per pages

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,6 +87,12 @@ DATABASES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
